b1bd08449956ab0a6534a457b8993daa723be717
sonalithorat <sonalithorat17@gmail.com>
2016-11-22 12:37:08 +0530

    Update Color.java

--- Color.java
+++ Color.java
@@ -1,16 +1,16 @@
 
 class Color
 {
-	int blue,green,red;
+	int blue,green,red1;
 	Color()
 	{
 		blue=10;
 		green=10;
-		red=10;
+		red1=10;
 	}
 	void display()
 	{
-		System.out.println("red="+red+",blue="+blue);
+		System.out.println("red1="+red1+",blue="+blue);
 	}
 	
 	
